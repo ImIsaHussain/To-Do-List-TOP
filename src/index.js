99b79f50c06e30ebe0b3c934d65a3239daf93af7
Isa Hussain <106785348+ImIsaHussain@users.noreply.github.com>
2024-09-07 15:12:58 -0400

    Add base template

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// making a todo list app
+
+// 1. add a new (todo / project) (input field, add button)
+// 2. delete a (todo / project) (delete button)
+// 3. complete a (todo / project) (checkbox, line-through, grayed out)
+// 4. show all (todos / projects) (ul, li, class active then completed)
+// 5. sort by urgency (projects) (ul, li, class high medium low)
+// 6. show active (todos / projects) (ul, li, class active)
+// 7. show completed (todos / projects) (ul, li, class completed)
+
+// project / section (personal, team, school, work, etc.)
+
+// nav bar on the side
+// filer / search (today, week / upcoming, labels)
+// tag items (existing tag, new tag) (home, fitness, appointments, groceries)
+// urgency of items (high, medium, low)
+// date / time on items (from - to time, complete before / on date)
+
+console.log("Hello World")
